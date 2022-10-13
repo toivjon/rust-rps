b285e68a5845c61e2c97a913300b6ad51056fa24
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2022-10-13 07:42:00 +0300

    Added the main menu structure and option to quit the game.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io::{stdin};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Rock Paper Scissors");
+    loop {
+        println!("MENU");
+        println!("  1   play game");
+        println!("  2   quit game");
+        println!("Please enter selection:");
+
+        let mut selection = String::new();
+        stdin().read_line(&mut selection).expect("Invalid input.");
+
+        match selection.trim() {
+            "1" => println!("TODO start game"),
+            "2" => break,
+            x => println!("Invalid input: {x}"),
+        }
+    }
 }
