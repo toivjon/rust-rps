822c79095ed40fa4121f65592f62a07b9e3c8c28
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2022-10-13 08:01:16 +0300

    Added the first version of the actual game logic.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::io::{stdin};
+use rand::{self, Rng};
 
 fn main() {
     println!("Rock Paper Scissors");
+    let mut rng = rand::thread_rng();
     loop {
         println!("MENU");
         println!("  1   play game");
@@ -23,10 +25,22 @@ fn main() {
                     let mut selection = String::new();
                     stdin().read_line(&mut selection).expect("Invalid input.");
 
+                    let opponent_selection = match rng.gen_range(0..3) {
+                        0 => "r",
+                        1 => "p",
+                        _ => "s",
+                    };
+
                     match selection.trim() {
-                        "r" => println!("You selected rock!"),
-                        "p" => println!("You selected paper!"),
-                        "s" => println!("You selected scissors!"),
+                        "r" if opponent_selection == "p" => {println!("You selected rock and opponent selected paper. Opponent wins!"); break;},
+                        "r" if opponent_selection == "s" => {println!("You selected rock and opponent selected scissors. You win!"); break;},
+                        "r" if opponent_selection == "r" => println!("You selected rock and opponent selected rock. It's a draw!"),
+                        "p" if opponent_selection == "s" => {println!("You selected paper and opponent selected scissors. Opponent wins!"); break;},
+                        "p" if opponent_selection == "r" => {println!("You selected paper and opponent selected rock. You win!"); break;},
+                        "p" if opponent_selection == "p" => println!("You selected paper and opponent selected paper. It's a draw!"),
+                        "s" if opponent_selection == "r" => {println!("You selected scissors and opponent selected rock. Opponent wins!");break;},
+                        "s" if opponent_selection == "p" => {println!("You selected scissors and opponent selected paper. You win!"); break;},
+                        "s" if opponent_selection == "p" => println!("You selected scissors and opponent selected scissors. It's a draw!"),
                         _ => break,
                     }
                 }
