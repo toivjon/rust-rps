fe0a3b46e9f631a35aa5ea9b2f1e3973feca8c41
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2022-10-13 19:50:57 +0300

    Applied code auto-format to main.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io::{stdin};
 use rand::{self, Rng};
+use std::io::stdin;
 
 fn main() {
     println!("Rock Paper Scissors");
@@ -14,35 +14,61 @@ fn main() {
         stdin().read_line(&mut selection).expect("Invalid input.");
 
         match selection.trim() {
-            "1" => {
-                loop {
-                    println!("GAME");
-                    println!("  r   rock");
-                    println!("  p   paper");
-                    println!("  s   scissors");
-                    println!("Please enter selection:");
+            "1" => loop {
+                println!("GAME");
+                println!("  r   rock");
+                println!("  p   paper");
+                println!("  s   scissors");
+                println!("Please enter selection:");
 
-                    let mut selection = String::new();
-                    stdin().read_line(&mut selection).expect("Invalid input.");
+                let mut selection = String::new();
+                stdin().read_line(&mut selection).expect("Invalid input.");
 
-                    let opponent_selection = match rng.gen_range(0..3) {
-                        0 => "r",
-                        1 => "p",
-                        _ => "s",
-                    };
+                let opponent_selection = match rng.gen_range(0..3) {
+                    0 => "r",
+                    1 => "p",
+                    _ => "s",
+                };
 
-                    match selection.trim() {
-                        "r" if opponent_selection == "p" => {println!("You selected rock and opponent selected paper. Opponent wins!"); break;},
-                        "r" if opponent_selection == "s" => {println!("You selected rock and opponent selected scissors. You win!"); break;},
-                        "r" if opponent_selection == "r" => println!("You selected rock and opponent selected rock. It's a draw!"),
-                        "p" if opponent_selection == "s" => {println!("You selected paper and opponent selected scissors. Opponent wins!"); break;},
-                        "p" if opponent_selection == "r" => {println!("You selected paper and opponent selected rock. You win!"); break;},
-                        "p" if opponent_selection == "p" => println!("You selected paper and opponent selected paper. It's a draw!"),
-                        "s" if opponent_selection == "r" => {println!("You selected scissors and opponent selected rock. Opponent wins!");break;},
-                        "s" if opponent_selection == "p" => {println!("You selected scissors and opponent selected paper. You win!"); break;},
-                        "s" if opponent_selection == "p" => println!("You selected scissors and opponent selected scissors. It's a draw!"),
-                        _ => break,
+                match selection.trim() {
+                    "r" if opponent_selection == "p" => {
+                        println!("You selected rock and opponent selected paper. Opponent wins!");
+                        break;
                     }
+                    "r" if opponent_selection == "s" => {
+                        println!("You selected rock and opponent selected scissors. You win!");
+                        break;
+                    }
+                    "r" if opponent_selection == "r" => {
+                        println!("You selected rock and opponent selected rock. It's a draw!")
+                    }
+                    "p" if opponent_selection == "s" => {
+                        println!(
+                            "You selected paper and opponent selected scissors. Opponent wins!"
+                        );
+                        break;
+                    }
+                    "p" if opponent_selection == "r" => {
+                        println!("You selected paper and opponent selected rock. You win!");
+                        break;
+                    }
+                    "p" if opponent_selection == "p" => {
+                        println!("You selected paper and opponent selected paper. It's a draw!")
+                    }
+                    "s" if opponent_selection == "r" => {
+                        println!(
+                            "You selected scissors and opponent selected rock. Opponent wins!"
+                        );
+                        break;
+                    }
+                    "s" if opponent_selection == "p" => {
+                        println!("You selected scissors and opponent selected paper. You win!");
+                        break;
+                    }
+                    "s" if opponent_selection == "p" => println!(
+                        "You selected scissors and opponent selected scissors. It's a draw!"
+                    ),
+                    _ => break,
                 }
             },
             "2" => break,
