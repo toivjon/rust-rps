c3768efbd5911675d2803da18bf6fe95dc2a2aa3
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2022-10-13 07:45:52 +0300

    Added the game layer placeholder.

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,25 @@ fn main() {
         stdin().read_line(&mut selection).expect("Invalid input.");
 
         match selection.trim() {
-            "1" => println!("TODO start game"),
+            "1" => {
+                loop {
+                    println!("GAME");
+                    println!("  r   rock");
+                    println!("  p   paper");
+                    println!("  s   scissors");
+                    println!("Please enter selection:");
+
+                    let mut selection = String::new();
+                    stdin().read_line(&mut selection).expect("Invalid input.");
+
+                    match selection.trim() {
+                        "r" => println!("You selected rock!"),
+                        "p" => println!("You selected paper!"),
+                        "s" => println!("You selected scissors!"),
+                        _ => break,
+                    }
+                }
+            },
             "2" => break,
             x => println!("Invalid input: {x}"),
         }
